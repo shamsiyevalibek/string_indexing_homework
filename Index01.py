c7202ebfc3b3bbbf4dd27249ed7a6d341a607d4d
unknown <shamsiyevalibek90@gmail.com>
2025-04-29 21:43:59 +0300

    sholl

--- Index01.py
+++ Index01.py
@@ -1,9 +1,15 @@
 def main(s):
     """
-    The string variable s is given. return the first character.
+    The string variable s is given. Return the first character.
+    If the string is empty, return "Empty string".
+    
     Args:
         s(str): parameter
     Returns:
         str: answer
     """
-    return
+    if s == "":
+        return "Empty string"
+    else:
+        return s[0]
+print(main("salom"))
